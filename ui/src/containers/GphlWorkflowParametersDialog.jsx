5a88cd7def94371b5b978e2ff21e22c9d54caea6
Dominika Trojanowska <dominika.trojanowska@s2innovation.com>
2024-05-07 11:20:37 +0200

    fixing cognitive complexity with eslint

--- ui/src/containers/GphlWorkflowParametersDialog.jsx
+++ ui/src/containers/GphlWorkflowParametersDialog.jsx
@@ -174,8 +174,8 @@ function GphlWorkflowParametersDialog(props) {
       e.target.type === 'number'
         ? Number(e.target.value)
         : e.target.type === 'checkbox'
-        ? e.target.checked
-        : e.target.value;
+          ? e.target.checked
+          : e.target.value;
 
     const form = e.currentTarget;
 
--- ui/src/serverIO.js
+++ ui/src/serverIO.js
@@ -135,7 +135,7 @@ class ServerIO {
 
   // eslint-disable-next-line sonarjs/cognitive-complexity
   listen() {
-    this.refreshInterval = setInterval(sendRefreshSession, 300000); // 5min
+    this.refreshInterval = setInterval(sendRefreshSession, 300_000); // 5min
     this.connect();
 
     if (this.initialized) {
